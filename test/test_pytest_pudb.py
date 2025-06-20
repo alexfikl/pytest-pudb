7a374a623dfbf2123ae6ede21ecdccdae99981e8
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2025-06-20 16:09:28 +0300

    fix(pytest_exception_interact): remove call to self.disable_io_capture
    https://github.com/wronglink/pytest-pudb/pull/26

--- test/test_pytest_pudb.py
+++ test/test_pytest_pudb.py
@@ -1,7 +1,35 @@
+import re
+import shutil
+
+import pytest
+
 pytest_plugins = "pytester"
 
 HELP_MESSAGE = "\\?\\:help"
 VARIABLES_TABLE = "V\x1b\\[0;30;47mariables:"
+
+# pudb/debugger.py:DebuggerUI#event_loop#WELCOME_LEVEL:2453
+DEFAULT_CONFIG = """\
+[pudb]
+prompt_on_quit = False
+seen_welcome = e999
+"""
+
+
+@pytest.fixture(autouse=True)
+def pudb_xdg_home(tmp_path_factory, monkeypatch):
+    configdir = tmp_path_factory.mktemp("pytest_pudb_testdir")
+    monkeypatch.setenv("XDG_CONFIG_HOME", configdir)
+
+    pudbconfig = configdir / "pudb" / "pudb.cfg"
+    pudbconfig.parent.mkdir(exist_ok=True)
+
+    with open(pudbconfig, "w", encoding="utf-8") as f:
+        f.write(DEFAULT_CONFIG)
+
+    yield
+
+    shutil.rmtree(configdir, ignore_errors=True)
 
 
 def test_pudb_interaction(testdir):
@@ -57,3 +85,33 @@ def test_pudb_b_integration(testdir):
     child.expect(HELP_MESSAGE)
     child.expect(VARIABLES_TABLE)
     child.sendeof()
+
+
+def test_pudb_avoid_double_prologue(testdir):
+    import pexpect
+
+    from pytest_pudb import ENTER_MESSAGE
+
+    p1 = testdir.makepyfile("""
+        def test_1():
+            test = []
+            assert test[0]
+    """)
+
+    re_escape = re.escape(ENTER_MESSAGE).encode("utf-8")
+    re_enter = re.compile(rb"\r\n>+ %s >+\r\n" % re_escape)  # \r\n instead of ^,$
+
+    child = testdir.spawn_pytest(f"--pudb {p1}")
+    child.expect(re_enter)
+    child.expect("PuDB")
+    child.expect(HELP_MESSAGE)
+    # Check that traceback postmortem handled
+    child.expect("PROCESSING EXCEPTION")
+    child.expect(VARIABLES_TABLE)
+
+    # Exit pudb
+    child.write("q")
+    ret = child.expect([re_enter, pexpect.EOF])
+
+    if ret == 0:
+        raise RuntimeError(f"pexpect found {re_enter!r} again!")
